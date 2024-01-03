8962486ff464c6a6abaa5a3f917177f12fd3d877
Lasith Dissanayake <86358728+DissanayakeLYB@users.noreply.github.com>
2024-01-03 22:59:38 +0530

    react- day2

--- react-somes/src/App.js
+++ react-somes/src/App.js
@@ -1,15 +1,28 @@
-import './App.scss';
-import { Routes, Route } from 'react-router-dom'
-import Sidebar from './components/Sidebar'
+import React from 'react';
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Sidebar from './components/Sidebar';
+import About from './pages/About.jsx';
+import Home from './pages/Home.jsx';
+import Events from './pages/Events.jsx';
+import Academics from './pages/Academics.jsx';
+import Contact from './pages/Contact.jsx';
 
-function App() {
+const App = () => {
   return (
-    <>
-    <Routes>
-      <Route path="/" element={<Sidebar/>} />
-    </Routes>
-    </>
+    <BrowserRouter>
+      <Sidebar>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Home" element={<Home />} />
+          <Route path="/About" element={<About />} />
+          <Route path="/Events" element={<Events />} />
+          <Route path="/Academics" element={<Academics />} />
+          <Route path="/Contact" element={<Contact />} />
+        </Routes>
+      </Sidebar>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
