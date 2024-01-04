3388cdb83ac9ee977288a7908787c251af9506ca
Lasith Dissanayake <86358728+DissanayakeLYB@users.noreply.github.com>
2024-01-04 22:49:52 +0530

    Update About.jsx

--- react-somes/src/pages/About.jsx
+++ react-somes/src/pages/About.jsx
@@ -3,18 +3,46 @@ import React from 'react';
 const About = () => {
     return (
         <div>
+
             <h1>About</h1>
+
             <div class='Vision'>
                 <h2>Our Vision</h2>
                 <p>Becoming a centre of excellence in higher learning, research, industrial consultancy and other relavant activities in Materials Science and Engineering.</p>
             </div>
+
             <div class='Mission'>
                 <h2>Our Mission</h2>
                 <p>To be a centre of excellence in Materials Science & Engineering by providing opportunity.</p>
+
                 <ul>
                     <li>For undergraduates and postgraduates to acquire specialized knowledge on the development and processing of engineering materials with attention to economic and environmental aspects while studying design, management and information technology.</li>
+
                     <li>To conduct high-quality research to cater for national development</li>
+
                     <li>To provide consultancy services to the public and private sector in specific and soceity in general.</li>
+                </ul>
+            </div>
+
+            <div class='aca-staff'>
+                <h2>Academic Staff</h2>
+
+                <ul>
+
+                    <li>Mr. V. Sivahar</li>
+                    <p>Student Counsellor, Head, Department of Materials Science and Engineering, Director/ Quality Assuarance Chairman/IQAU</p>
+
+                    <li>Prof. S.U</li>
+                    <li>Mr. V. Sivahar</li>
+                    <li>Mr. V. Sivahar</li>
+                    <li>Mr. V. Sivahar</li>
+                    <li>Mr. V. Sivahar</li>
+                    <li>Mr. V. Sivahar</li>
+                    <li>Mr. V. Sivahar</li>
+                    <li>Mr. V. Sivahar</li>
+                    <li>Mr. V. Sivahar</li>
+                    <li>Mr. V. Sivahar</li>
+                    <li>Mr. V. Sivahar</li>
                 </ul>
             </div>
         </div>
