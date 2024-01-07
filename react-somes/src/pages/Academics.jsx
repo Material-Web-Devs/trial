a9265b1d2ff65d9e6418f743c36daf145b5545c0
Lasith Dissanayake <86358728+DissanayakeLYB@users.noreply.github.com>
2024-01-07 14:09:32 +0530

    Update Academics.jsx

--- react-somes/src/pages/Academics.jsx
+++ react-somes/src/pages/Academics.jsx
@@ -5,30 +5,35 @@ const Academics = () => {
         <div>
             <div>
                 <h1>Academics</h1>
-
+                <br></br>
                 <h2>Postgradute Programs</h2>
-
+                <br></br>
                 <h3>Postgraduate Courses</h3>
-
+                <br></br>
                 <p>Department of Materials Science & Engineering offers two programmes.</p>
 
                 <ul>
                     <li>M.Sc./PG Dip in Materials Science</li>
                     <li>M.Sc./PG Dip in Nanotechnology</li>
                 </ul>
-
+                <br></br>
+                <br></br>
                 <h3>Postgraduate Research Degrees</h3>
-
+                <br></br>            
                 <ul>
                     <li>M.Sc. (Full time/Part time)</li>
                     <li>M.Phil. (Full time/Part time)</li>
                     <li>Ph.D. (Full time/Part time)</li>
                 </ul>
             </div>         
-        
+            
+            <br></br>
+            <br></br>
+
             <div class="2018-intake-projects">
 
                 <h3>2018 Intake Research Projects</h3>
+                <br></br>
 
                 <table>
                     <tr>
@@ -168,11 +173,14 @@ const Academics = () => {
 
                 </table>
             </div>
+
+            <br></br>
+            <br></br>
 
             <div class="2017-intake-projects">
 
                 <h3>2017 Intake Research Projects</h3>
-
+                <br></br>
                 <table>
                     <tr>
                         <th>Research Project Title</th>
@@ -180,141 +188,135 @@ const Academics = () => {
                     </tr>
 
                     <tr>
-                        <td>Development of advanced polymer composites using natural resins</td>
-                        <td>Mr.AMPB Samarasekara</td>
-                    </tr>
-
-                    <tr>
-                        <td>Development of cellulose based nano-fibers by electrospinning techniques</td>
-                        <td>Mr.AMPB Samarasekara</td>
-                    </tr>
-
-                    <tr>
-                        <td>Development of open-source software for polymer composites analysis</td>
-                        <td>Mr AMPB Samarasekara</td>
-                    </tr>
-
-                    <tr>
-                        <td>Extraction of natural dyes from selected plant sources and its application in fabrics</td>
-                        <td>Mr SV Udayakumara</td>
-                    </tr>
-
-                    <tr>
-                        <td>Development of new bio-based adhesive for paper/cardboard</td>
-                        <td>Mr SV Udayakumara</td>
-                    </tr>
-
-                    <tr>
-                        <td>Preparation and characterization of LDPE based wood-plastic composites reinforced with clay.</td>
-                        <td>Mr SV Udayakumara</td>
-                    </tr>
-
-                    <tr>
-                        <td>Preparation Of High-Quality Graphene Oxide From Sri Lankan Vein Graphite</td>
-                        <td>Dr.HS Sitinamaluwa</td>
-                    </tr>
-
-                    <tr>
-                        <td>Activated graphene oxide for removal of organic pollutant from wastewater</td>
-                        <td>Dr.HS Sitinamaluwa</td>
-                    </tr>
-
-                    <tr>
-                        <td>Optimization of the Extraction of Nano CaO powder from eggshells for agricultural applications</td>
-                        <td>Prof. AS Galhenage</td>
-                    </tr>
-
-                    <tr>
-                        <td>Synthesis of Fluoride-Containing Varnish for Application to the tooth surface</td>
-                        <td>Prof. AS Galhenage</td>
-                    </tr>
-
-                    <tr>
-                        <td>Development of petroleum oil quality analyser based on optical metrological techniques</td>
-                        <td>Dr D Attygalle</td>
-                    </tr>
-
-                    <tr>
-                        <td>Influence of particle size on biocompatibility of hydroxyapatite nanoparticles</td>
-                        <td>Prof. SU Adikary</td>
-                    </tr>
-
-                    <tr>
-                        <td>Numerical optimization of band gap gadient of CIGS solar cell</td>
-                        <td>Dr.D Attygalle</td>
-                    </tr>
-
-                    <tr>
-                        <td>Modeling and Simulation of Solid State Micro-pump Based on Piezoelectric Ceramics</td>
-                        <td>Prof. SU Adikary</td>
-                    </tr>
-
-                    <tr>
-                        <td>Modeling and Simulation of piezoelectric nano generators based on nano wire arrays</td>
-                        <td>Prof. SU Adikary</td>
-                    </tr>
-
-                    <tr>
-                        <td>Development of an exhaled breath analyzer for early detection of diseases</td>
-                        <td>Dr DAS Amarasinghe</td>
-                    </tr>
-
-                    <tr>
-                        <td>Studying the effect of aging temperature and quenching medium on the hardness of AL6063 Alloy</td>
-                        <td>Dr.GIP De Silva</td>
-                    </tr>
-
-                    <tr>
-                        <td>Development of low cost bricks using waste foundary dust</td>
-                        <td>Dr.GIP De Silva</td>
-                    </tr>
-
-                    <tr>
-                        <td>Estimation of Weibull modulus through ultrasonic velocity measurements</td>
-                        <td>Mr.V Sivahar</td>
-                    </tr>
-
-                    <tr>
-                        <td>Monitoring of FRP degradation with ultrasound</td>
-                        <td>Mr.V Sivahar</td>
-                    </tr>
-
-                    <tr>
-                        <td>Dynamic Mechanical Analysis of Elastomers</td>
-                        <td>Mr.VSC Weragoda</td>
-                    </tr>
-
-                    <tr>
-                        <td>Measurement of effective heat transfer in elastomers</td>
-                        <td>Mr.VSC Weragoda</td>
-                    </tr>
-
-                    <tr>
-                        <td>Conductive Elastomeric Sensors</td>
-                        <td>Mr.VSC Weragoda</td>
-                    </tr>
-
-                    <tr>
-                        <td>Numerical Prediction of Curing Status of Graphite based Tire Compounds under Vulcanization</td>
+                        <td>Characterization of the Deformation and Damage of Biological Soft Tissues for Tissue Engineering</td>
                         <td>Dr AAGA Abeygunawardane</td>
                     </tr>
 
                     <tr>
-                        <td>Suitability of using Al sludge waste to produce paving block</td>
-                        <td>Mr.SP Guluwita</td>
+                        <td>Preparation and Characterization of Porous Graphene from Sri Lanka Vein Graphite.</td>
+                        <td>Dr (Mrs) HS Sitinamaluwa</td>
                     </tr>
 
                     <tr>
-                        <td>Suitability of  extracting aluminium from aluminium sludge waste</td>
-                        <td>Mr.SP Guluwita</td>
+                        <td>Development of Nano-Cellulose based Respirator Filters</td>
+                        <td>Mr AMPB Samarasekara</td>
+                    </tr>
+
+                    <tr>
+                        <td>Quantitative Estimation of Residual Stresses in Quenched Steel through Ultrasonic Parameters.</td>
+                        <td>Mr V Sivahar</td>
+                    </tr>
+
+                    <tr>
+                        <td>Modelling of Cement - Waste Paper - Natural Fiber based Composite for Ceiling Sheet/Partition Board</td>
+                        <td>Mr SV Udayakumara</td>
+                    </tr>
+
+                    <tr>
+                        <td>Numerical simulation of Lead-Free bismuth perovskite  by using SCAPS (Solar Cell Capacitance Simulator)</td>
+                        <td>Dr AS Galhenage</td>
+                    </tr>
+
+                    <tr>
+                        <td>Characterization of Viscoelastic Materials by Ultrasonic  through Transmission Technique</td>
+                        <td>Mr V Sivahar</td>
+                    </tr>
+
+                    <tr>
+                        <td>Environmental Corrosion on Structural Steel. </td>
+                        <td>Eng S.P Guluwita</td>
+                    </tr>
+
+                    <tr>
+                        <td>Nitrogen doped Graphene for Energy Storage</td>
+                        <td>Dr (Mrs) HS Sitinamaluwa</td>
+                    </tr>
+
+                    <tr>
+                        <td>Modelling, Optimization and Design of Efficient  Piezoelectric Energy Harvesting Device to Power  Wearable Electronics.</td>
+                        <td>Prof S.U Adikari</td>
+                    </tr>
+
+                    <tr>
+                        <td>Design and Fabrication of Bi-Axial Tensile Testing Machine  to Characterize Polymer Composite.</td>
+                        <td>Dr AAGA Abeygunawardane</td>
+                    </tr>
+
+                    <tr>
+                        <td>A Cure Monitoring Method Development for Adhesives using Direct Current Resistivity</td>
+                        <td>Mr.V.S.C Weragoda</td>
+                    </tr>
+
+                    <tr>
+                        <td>Development of Magnetic Tracebility Sensor.</td>
+                        <td>Dr.D Attygalle</td>
+                    </tr>
+
+                    <tr>
+                        <td>Synthesis of Nano-Calcium Oxide from Eggshell for Dental  Applications.</td>
+                        <td>Dr (Mrs).A.S.Galhenage</td>
+                    </tr>
+
+                    <tr>
+                        <td>Extraction of Copper and Sand from Blasting Sand</td>
+                        <td>Eng.S.P Guluwita</td>
+                    </tr>
+
+                    <tr>
+                        <td>Study on Plasma based Printability Improvement. </td>
+                        <td>Dr DAS Amarasinghe</td>
+                    </tr>
+
+                    <tr>
+                        <td>Design of a Bridge Expansion Joint</td>
+                        <td>Mr.V.S.C Weragoda</td>
+                    </tr>
+
+                    <tr>
+                        <td>Simulation Study on the Effect of Fillers on the Mechanical Properties of Thermoplastics</td>
+                        <td>Mr SV Udayakumara</td>
+                    </tr>
+
+                    <tr>
+                        <td>Development of Soil Based Composite Wall Panels Using Locally Available Laterite Soil	</td>
+                        <td>Dr. D Attygalle</td>
+                    </tr>
+
+                    <tr>
+                        <td>Design and Optimization of Linear Actuator for  Biomedical Applications. 	</td>
+                        <td>Prof.S.U Adikari</td>
+                    </tr>
+
+                    <tr>
+                        <td>Characterization of Cu/Ni(Mn) Foil used in Strain Gauge Industry</td>
+                        <td>Mr.VSC Weragoda</td>
+                    </tr>
+
+                    <tr>
+                        <td>Synthesizing and Characterization of Composites with  Palmyrah Leaf Petioles as a Construction Materials.</td>
+                        <td>Eng.S.P.Guluwita</td>
+                    </tr>
+
+                    <tr>
+                        <td>Explore the suitability of M-sand sludge waste in the production of paving blocks and its mix design</td>
+                        <td>Eng.S.P.Guluwita</td>
+                    </tr>
+
+                    <tr>
+                        <td>Optimization of process parameters to improve the tensile strength of Aluminum alloy 6201</td>
+                        <td>Eng.S.P.Guluwita</td>
                     </tr>
 
                 </table>
             </div>
+        
+            <br></br>
+            <br></br>
 
             <div class="2016-intake-projects">
 
                 <h3>2016 Intake Research Projects</h3>
+                <br></br>
 
                 <table>
                     <tr>
