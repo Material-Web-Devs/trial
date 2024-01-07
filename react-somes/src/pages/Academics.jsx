f7c9080b16be2eda2318a380844cf8678cfb2e96
Lasith Dissanayake <86358728+DissanayakeLYB@users.noreply.github.com>
2024-01-07 13:52:04 +0530

    Update Academics.jsx

--- react-somes/src/pages/Academics.jsx
+++ react-somes/src/pages/Academics.jsx
@@ -3,26 +3,218 @@ import React from 'react';
 const Academics = () => {
     return (
         <div>
-            <h1>Academics</h1>
+            <div>
+                <h1>Academics</h1>
 
-            <h2>Postgradute Programs</h2>
+                <h2>Postgradute Programs</h2>
 
-            <h3>Postgraduate Taught Courses</h3>
+                <h3>Postgraduate Courses</h3>
 
-            <p>Department of Materials Science & Engineering offers two programmes.</p>
+                <p>Department of Materials Science & Engineering offers two programmes.</p>
 
-            <ul>
-                <li>M.Sc./PG Dip in Materials Science</li>
-                <li>M.Sc./PG Dip in Nanotechnology</li>
-            </ul>
+                <ul>
+                    <li>M.Sc./PG Dip in Materials Science</li>
+                    <li>M.Sc./PG Dip in Nanotechnology</li>
+                </ul>
 
-            <h3>Postgraduate Research Degrees</h3>
+                <h3>Postgraduate Research Degrees</h3>
 
-            <ul>
-                <li>M.Sc. (Full time/Part time)</li>
-                <li>M.Phil. (Full time/Part time)</li>
-                <li>Ph.D. (Full time/Part time)</li>
-            </ul>
+                <ul>
+                    <li>M.Sc. (Full time/Part time)</li>
+                    <li>M.Phil. (Full time/Part time)</li>
+                    <li>Ph.D. (Full time/Part time)</li>
+                </ul>
+            </div>         
+        
+            <div class="2018-intake-projects">
+                <table>
+                    <tr>
+                        <th>Research Project Title</th>
+                        <th>Supervisor(s)</th>
+                    </tr>
+
+                    <tr>
+                        <td>Development of advanced polymer composites using natural resins</td>
+                        <td>Mr.AMPB Samarasekara</td>
+                    </tr>
+
+                    <tr>
+                        <td>Development of cellulose based nano-fibers by electrospinning techniques</td>
+                        <td>Mr.AMPB Samarasekara</td>
+                    </tr>
+
+                    <tr>
+                        <td>Development of open-source software for polymer composites analysis</td>
+                        <td>Mr AMPB Samarasekara</td>
+                    </tr>
+
+                    <tr>
+                        <td>Extraction of natural dyes from selected plant sources and its application in fabrics</td>
+                        <td>Mr SV Udayakumara</td>
+                    </tr>
+
+                    <tr>
+                        <td>Development of new bio-based adhesive for paper/cardboard</td>
+                        <td>Mr SV Udayakumara</td>
+                    </tr>
+
+                    <tr>
+                        <td>Preparation and characterization of LDPE based wood-plastic composites reinforced with clay.</td>
+                        <td>Mr SV Udayakumara</td>
+                    </tr>
+
+                    <tr>
+                        <td>Preparation Of High-Quality Graphene Oxide From Sri Lankan Vein Graphite</td>
+                        <td>Dr.HS Sitinamaluwa</td>
+                    </tr>
+
+                    <tr>
+                        <td>Activated graphene oxide for removal of organic pollutant from wastewater</td>
+                        <td>Dr.HS Sitinamaluwa</td>
+                    </tr>
+
+                    <tr>
+                        <td>Optimization of the Extraction of Nano CaO powder from eggshells for agricultural applications</td>
+                        <td>Prof. AS Galhenage</td>
+                    </tr>
+
+                    <tr>
+                        <td>Synthesis of Fluoride-Containing Varnish for Application to the tooth surface</td>
+                        <td>Prof. AS Galhenage</td>
+                    </tr>
+
+                    <tr>
+                        <td>Development of petroleum oil quality analyser based on optical metrological techniques</td>
+                        <td>Dr D Attygalle</td>
+                    </tr>
+
+                    <tr>
+                        <td>Influence of particle size on biocompatibility of hydroxyapatite nanoparticles</td>
+                        <td>Prof. SU Adikary</td>
+                    </tr>
+
+                    <tr>
+                        <td>Numerical optimization of band gap gadient of CIGS solar cell</td>
+                        <td>Dr.D Attygalle</td>
+                    </tr>
+
+                    <tr>
+                        <td>Modeling and Simulation of Solid State Micro-pump Based on Piezoelectric Ceramics</td>
+                        <td>Prof. SU Adikary</td>
+                    </tr>
+
+                    <tr>
+                        <td>Modeling and Simulation of piezoelectric nano generators based on nano wire arrays</td>
+                        <td>Prof. SU Adikary</td>
+                    </tr>
+
+                    <tr>
+                        <td>Development of an exhaled breath analyzer for early detection of diseases</td>
+                        <td>Dr DAS Amarasinghe</td>
+                    </tr>
+
+                    <tr>
+                        <td>Studying the effect of aging temperature and quenching medium on the hardness of AL6063 Alloy</td>
+                        <td>Dr.GIP De Silva</td>
+                    </tr>
+
+                    <tr>
+                        <td>Development of low cost bricks using waste foundary dust</td>
+                        <td>Dr.GIP De Silva</td>
+                    </tr>
+
+                    <tr>
+                        <td>Estimation of Weibull modulus through ultrasonic velocity measurements</td>
+                        <td>Mr.V Sivahar</td>
+                    </tr>
+
+                    <tr>
+                        <td>Monitoring of FRP degradation with ultrasound</td>
+                        <td>Mr.V Sivahar</td>
+                    </tr>
+
+                    <tr>
+                        <td>Dynamic Mechanical Analysis of Elastomers</td>
+                        <td>Mr.VSC Weragoda</td>
+                    </tr>
+
+                    <tr>
+                        <td>Measurement of effective heat transfer in elastomers</td>
+                        <td>Mr.VSC Weragoda</td>
+                    </tr>
+
+                    <tr>
+                        <td>Conductive Elastomeric Sensors</td>
+                        <td>Mr.VSC Weragoda</td>
+                    </tr>
+
+                    <tr>
+                        <td>Numerical Prediction of Curing Status of Graphite based Tire Compounds under Vulcanization</td>
+                        <td>Dr AAGA Abeygunawardane</td>
+                    </tr>
+
+                    <tr>
+                        <td>Suitability of using Al sludge waste to produce paving block</td>
+                        <td>Mr.SP Guluwita</td>
+                    </tr>
+
+                    <tr>
+                        <td>Suitability of  extracting aluminium from aluminium sludge waste</td>
+                        <td>Mr.SP Guluwita</td>
+                    </tr>
+
+                </table>
+            </div>
+
+            <div class="2017-intake-projects">
+                <table>
+                    <tr>
+                        <th>Research Project Title</th>
+                        <th>Supervisor(s)</th>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                </table>
+            </div>
+
+            <div class="2016-intake-projects">
+                <table>
+                    <tr>
+                        <th>Research Project Title</th>
+                        <th>Supervisor(s)</th>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                </table>
+            </div>
         </div>
     );
 };
